bea4a6a7adc580ae2145cc3b3fae3d31335237b3
Prithvi Abhishetty <prithviabhishetty@gmail.com>
2026-03-12 17:49:27 +0000

    fix to allow pytests to run from repository root

--- tests/test_api.py
+++ tests/test_api.py
@@ -2,8 +2,6 @@ from fastapi.testclient import TestClient
 import os
 import sys
 
-sys.path.append(os.path.join(os.path.dirname(__file__), '../api'))
-
 from api.main import app
 
 client = TestClient(app)
